34750e62ee36e6d132269fef6d875094ee7bd198
weiyuexin <3022422894@qq.com>
2023-02-07 18:28:47 +0800

    init

---
+++ src/main/java/top/weiyuexin/OnlineCodeEvaluationSystemApplication.java
@@ -0,0 +1,13 @@
+package top.weiyuexin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCodeEvaluationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineCodeEvaluationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/weiyuexin/OnlineCodeEvaluationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package top.weiyuexin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCodeEvaluationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

7a54a3e5b1add143443b847ff6ab96d6595d7ded
weiyuexin <3022422894@qq.com>
2023-03-07 23:48:49 +0800

    controller

--- src/main/java/top/weiyuexin/controller/ArticleController.java
+++ src/main/java/top/weiyuexin/controller/ArticleController.java
@@ -1,8 +1,12 @@
 package top.weiyuexin.controller;
 
+import io.swagger.models.auth.In;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import top.weiyuexin.pojo.Article;
+import top.weiyuexin.pojo.vo.R;
+import top.weiyuexin.service.ArticleService;
 
 /**
  * @PackageName: top.weiyuexin.controller
@@ -15,4 +19,56 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/article")
 public class ArticleController {
+    @Autowired
+    private ArticleService articleService;
+    /**
+     * 根据id查询文字
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    public R getById(@PathVariable("id") String id){
+        return R.success();
+    }
+
+    /**
+     * 分页查询
+     * @param start
+     * @param pageSize
+     * @return
+     */
+    @GetMapping("/list")
+    public R getPage(Integer start, Integer pageSize){
+        return R.success();
+    }
+
+    /**
+     * 发布文章
+     * @param article
+     * @return
+     */
+    @PostMapping("/add")
+    public R addArticle(Article article){
+        return R.success();
+    }
+
+    /**
+     * 修改文章
+     * @param article
+     * @return
+     */
+    @PostMapping("/update")
+    public R updateArticle(Article article){
+        return R.success();
+    }
+
+    /**
+     * 删除文章
+     * @param article
+     * @return
+     */
+    @DeleteMapping("/delete")
+    public R deleteArticle(Article article){
+        return R.success();
+    }
 }
--- src/main/java/top/weiyuexin/controller/UserController.java
+++ src/main/java/top/weiyuexin/controller/UserController.java
@@ -4,8 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import top.weiyuexin.pojo.Article;
 import top.weiyuexin.pojo.User;
+import top.weiyuexin.pojo.vo.R;
 import top.weiyuexin.service.UserService;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * @PackageName: top.weiyuexin.controller
@@ -22,9 +26,67 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    /**
+     * 根据id查询用户
+     * @param id
+     * @return
+     */
     @GetMapping("/{id}")
     @ResponseBody
-    public User findById(@PathVariable("id") Integer id){
-        return userService.getById(id);
+    public R findById(@PathVariable("id") Integer id){
+        return R.success(userService.getById(id));
+    }
+
+    /**
+     * 分页查询
+     * @param start
+     * @param pageSize
+     * @return
+     */
+    @GetMapping("/list")
+    public R getPage(Integer start, Integer pageSize){
+        return R.success();
+    }
+
+
+    /**
+     * 注册
+     * @param user
+     * @return
+     */
+    @PostMapping("/register")
+    public R register(User user){
+        return R.success();
+    }
+
+    /**
+     * 登录
+     * @param user
+     * @param session
+     * @return
+     */
+    @GetMapping("/login")
+    public R login(User user,HttpSession session){
+        return R.success();
+    }
+
+    /**
+     * 修改用户信息
+     * @param user
+     * @return
+     */
+    @PostMapping("/update")
+    public R updateUser(User user){
+        return R.success();
+    }
+
+    /**
+     * 删除用户
+     * @param user
+     * @return
+     */
+    @DeleteMapping("/delete")
+    public R deleteArticle(User user){
+        return R.success();
     }
 }

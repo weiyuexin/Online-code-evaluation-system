0e07d99ab41329953165697a5d93dd4472cc9733
weiyuexin <3022422894@qq.com>
2023-03-19 22:15:21 +0800

    开启spring boot热部署

--- src/main/java/top/weiyuexin/Application.java
+++ src/main/java/top/weiyuexin/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+//mvn spring-boot:run 根目录下运行
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
